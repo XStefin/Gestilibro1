d8b9a1f6964b5798f2125aceb30e9aff5d958a26
XStefin <michael.caldas.sterpin@gmail.com>
2026-04-02 19:05:10 -0500

    Solucion de 3 primeros errores

--- GestiLibro/app/Config/Routes.php
+++ GestiLibro/app/Config/Routes.php
@@ -6,7 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-$routes->get('/','Home::index');
+$routes->get('/','Login::index');
 $routes->get('/login', 'Login::index');
 $routes->post('/login/auth', 'Login::auth');
 $routes->get('/dashboard', 'DashboardController::index');
