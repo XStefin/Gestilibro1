076b754840e83bb80fcf4bcdce15320b160a40d7
Upwarp <159637736+Upwarp@users.noreply.github.com>
2026-05-18 13:20:25 -0500

    Actualización para despliegue

--- GestiLibro/app/Config/Routes.php
+++ GestiLibro/app/Config/Routes.php
@@ -1,34 +1,24 @@
 <?php
 
-
 use CodeIgniter\Router\RouteCollection;
-
 
 /**
  * @var RouteCollection $routes
  */
 
-
-$routes->options('api', static function () {
-    return service('response')->setStatusCode(200);
+$routes->options('(:any)', static function () {
+    return service('response')
+        ->setHeader('Access-Control-Allow-Origin', 'https://front-gestilibro.vercel.app')
+        ->setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+        ->setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin')
+        ->setHeader('Access-Control-Allow-Credentials', 'true')
+        ->setStatusCode(200);
 });
-
-
-$routes->options('api/(:segment)', static function () {
-    return service('response')->setStatusCode(200);
-});
-
-
-$routes->options('api/(:segment)/(:segment)', static function () {
-    return service('response')->setStatusCode(200);
-});
-
 
 $routes->post('api/login', 'Login::auth');
 $routes->post('api/logout', 'Login::logout');
 $routes->post('api/verifypin', 'UsuarioController::verifyPin');
 $routes->post('api/resendpin', 'UsuarioController::resendPin');
-
 
 $routes->group('api', ['namespace' => 'App\Controllers'], function ($routes) {
     $routes->resource('usuarios', ['controller' => 'UsuarioController']);
