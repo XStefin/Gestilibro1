28097f0a80ad6bf754c1952fe650aa7f5dfe15cc
michael caldas sterpin <159655689+XStefin@users.noreply.github.com>
2026-05-28 18:22:22 -0500

    Update UsuarioController.php

--- GestiLibro/app/Controllers/UsuarioController.php
+++ GestiLibro/app/Controllers/UsuarioController.php
@@ -182,7 +182,7 @@ class UsuarioController extends ResourceController
         $pinAnterior = $usuario['pin'] ?? '';
         $activeAnterior = (int) ($usuario['active'] ?? 0);
 
-        $nuevoPin = str_pad((string) random_int(0, 999999), 6, '0', STR_PAD_LEFT);
+        $nuevoPin = str_pad((string) random_int(0, 9999), 4, '0', STR_PAD_LEFT);
 
         $this->model->update($usuario['id_usuario'], [
             'pin' => $nuevoPin,
@@ -335,7 +335,7 @@ Ingresa este PIN para activar tu cuenta.
         $esRegistro = isset($data['esRegistro']) ? (bool) $data['esRegistro'] : false;
 
         $pin = $esRegistro
-            ? str_pad((string) random_int(0, 999999), 6, '0', STR_PAD_LEFT)
+            ? str_pad((string) random_int(0, 9999), 4, '0', STR_PAD_LEFT)
             : trim($data['pin'] ?? '');
 
         $datosInsertar = [
